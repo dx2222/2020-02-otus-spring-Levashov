0d40b2df380d83aeaf38dee8776a8b6bbd1f0af6
dlevashov <dlevashov@diasoft.ru>
2020-04-07 17:48:08 +0300

    homework_02

--- homework_02/src/main/java/ru/otus/spring/Main.java
+++ homework_02/src/main/java/ru/otus/spring/Main.java
@@ -5,25 +5,13 @@ import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import ru.otus.spring.service.ExaminationService;
+import ru.otus.spring.config.*;
 
-
+@Import(MessageSourceConfig.class)
 @PropertySource("classpath:application.properties")
 @Configuration
 @ComponentScan
 public class Main {
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertyConfig(){
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
-        ms.setBasename("/i18n/bundle");
-        ms.setDefaultEncoding("UTF-8");
-        return ms;
-    }
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
---
+++ homework_02/src/main/java/ru/otus/spring/config/MessageSourceConfig.java
@@ -0,0 +1,18 @@
+package ru.otus.spring.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+        ms.setBasename("/i18n/bundle");
+        ms.setDefaultEncoding("UTF-8");
+        return ms;
+    }
+}
